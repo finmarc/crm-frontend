86254fc0afc65e7ec403c73101d311b77f64f829
Wilson Lucena <wilsonllucena@gmail.com>
2022-08-13 01:27:23 -0300

    feature: funcionalidade de edicao

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { Switch } from "react-router-dom";
 import SideMenu from "../layouts/side-menu/Main";
 import Colaborador from "../pages/colaboradores";
+import ColaboradorEdit from "../pages/colaboradores/edit";
 import { Form } from "../pages/colaboradores/Form";
 import ColaboradorDetails from "../pages/colaboradores/show";
 import Dashboard from "../pages/dashboard/Main";
@@ -11,13 +12,14 @@ const Router = () => (
   <>
     <Switch>
       <Route path="/login" component={Login} />
-
+''
       <SideMenu>
         <Route path="/" exact isPrivate component={Dashboard} />
         <Route path="/admin" isPrivate component={Dashboard} />
         <Route path="/colaboradores" isPrivate component={Colaborador} />
         <Route path="/colaborador/new" isPrivate component={Form} />
         <Route path="/colaborador/show/:id" isPrivate component={ColaboradorDetails} />
+        <Route path="/colaborador/edit/:id" isPrivate component={ColaboradorEdit} />
       </SideMenu>
     </Switch>
   </>
