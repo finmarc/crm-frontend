1cc4cdafbefb42b7b2918e906c8556c7d94fae51
Omar de Paiva <omar.paiva@gmail.com>
2022-09-20 10:40:35 -0300

    feat:  color adjustments on cards

--- src/components/CardUpload/index.tsx
+++ src/components/CardUpload/index.tsx
@@ -6,7 +6,7 @@ export default function CardUpload(props: any) {
   if (index === 6) {
     return (
       <div className="container flex mx-auto p-4 max-w-md">
-        <div className="p-6 bg-gray-50 rounded-lg border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 overflow-hidden border-2 h-full ">
+        <div className="p-6 bg-gray-50 rounded-lg border-blue-700 border-solid cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 overflow-hidden border-2 h-full ">
           <div className="lg:h-10 mb-12">
             <p className="text-base text-center font-semibold">{description}</p>
           </div>
@@ -18,7 +18,7 @@ export default function CardUpload(props: any) {
 
   return (
     <div className="container flex mx-auto p-4 max-w-md">
-      <div className="p-6 bg-gray-50 rounded-lg border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 overflow-hidden border-2 h-full ">
+      <div className="p-6 bg-gray-50 rounded-lg border-blue-700 border-solid cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 overflow-hidden border-2 h-full ">
         <div className="lg:h-10 mb-12 ">
           <p className="text-md text-center font-semibold ">{description}</p>
         </div>
