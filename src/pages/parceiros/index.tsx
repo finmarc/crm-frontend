07ed1cb46d47f01736625e91584af899ed021c5e
Omar de Paiva <omar.paiva@gmail.com>
2022-11-24 10:32:26 -0300

    feat: páginação dos parceiros

--- src/pages/parceiros/index.tsx
+++ src/pages/parceiros/index.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
+import DataTable from "../../components/Datatable";
 import { Header } from "../../components/Header";
 import { Table } from "../../components/Table";
 import api from "../../services/apiClient";
@@ -9,45 +10,71 @@ interface Partners {
   document: string;
   email: string;
   fone: string;
-  birth_date: Date,
-  rg?:string,
-  sexo?:string,
-  address?:string,
-  description?:string,
-  seller_id?:string,
+  birth_date: Date;
+  rg?: string;
+  sexo?: string;
+  address?: string;
+  description?: string;
+  seller_id?: string;
 }
 
 const Parceiro = () => {
   const [partners, setPartners] = useState<Partners[]>([]);
+  useEffect(() => {
+    window.process = {
+      ...window.process,
+    };
+  }, []);
 
   const fetchData = useCallback(async () => {
     const response = await api.get("/partners");
 
-    
     const newPartners: any = response.data.map((partner: Partners) => {
       return {
         id: partner.id,
-        nome:  partner.name,
-        email:  partner.email,
-        telefone:  partner.fone,
-        documento:  partner.document,
-        
-        
-      }
-    })
+        nome: partner.name,
+        email: partner.email,
+        telefone: partner.fone,
+        documento: partner.document,
+      };
+    });
 
     setPartners(newPartners);
   }, []);
-  const theadTitles = [
-    "id", "Nome", "Email", "Telefone", "CPF"
-  ]
+
+  const columns = [
+    {
+      field: "nome",
+      headerName: "Nome",
+      width: 220,
+    },
+    {
+      field: "email",
+      headerName: "E-mail",
+      width: 180,
+    },
+    {
+      field: "telefone",
+      headerName: "Telefone",
+      width: 150,
+    },
+    {
+      field: "documento",
+      headerName: "Documento",
+      width: 150,
+    },
+  ];
   useEffect(() => {
     fetchData();
   }, [fetchData]);
   return (
     <>
-      <Header title="Parceiros" url="parceiro" action="Adicionar" />
-      {partners.length > 0 && <Table titles={theadTitles} columns={partners} component="parceiro" url="partners" />}
+      <DataTable
+        rows={partners}
+        columns={columns}
+        component="parceiro"
+        url="parceiros"
+      />
     </>
   );
 };
