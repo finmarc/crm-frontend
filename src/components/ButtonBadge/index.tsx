49315a5555ce7c96f7ec2dfe2c166761aaa971e2
Omar de Paiva <omar.paiva@gmail.com>
2023-02-24 07:42:49 -0300

    feat: button badge

--- src/components/ButtonBadge/index.tsx
+++ src/components/ButtonBadge/index.tsx
@@ -3,37 +3,46 @@ export type ButtonBadgeProps = {
 };
 
 export default function ButtonBadge(props: ButtonBadgeProps) {
-  const BadgeColor: Record<string, string> = {
-    Aprovado: "btn-elevated-success",
-    Reprovado: "btn-elevated-danger",
-  };
-  console.log(props.situation);
+  let ClasseNameColor: string = "";
 
-  function Modification() {}
-  const ClasseNameColor = `btn ${
-    BadgeColor[props.situation]
-  }  w-24 mt-2 mr-1 mb-2`;
+  if (props.situation === "Pendente documentação") {
+    ClasseNameColor =
+      "bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300";
+  }
+  if (props.situation === "Pendência encontrada") {
+    ClasseNameColor =
+      "bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300";
+  }
+  if (props.situation === "Aprovado") {
+    ClasseNameColor =
+      "bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300";
+  }
+  if (props.situation === "Concluído") {
+    ClasseNameColor =
+      "bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300";
+  }
+
+  if (props.situation === "Reprovado") {
+    ClasseNameColor =
+      "bg-red-300 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300";
+  }
+  if (props.situation.includes("Análise")) {
+    ClasseNameColor =
+      " bg-indigo-300 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300";
+  }
+  if (props.situation === "Enviado para o banco") {
+    ClasseNameColor =
+      "bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300";
+  }
+
+  if (props.situation === "Pré-análise") {
+    ClasseNameColor =
+      "bg-gray-300 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300";
+  }
 
   return (
     <div>
-      <button className={ClasseNameColor}>{props.situation}</button>
-
-      {/* 
-      <button className="btn btn-elevated-warning w-24 mr-1 mb-2">
-        Pendente de documentação
-      </button>
-      <button className="btn btn-elevated-warning w-24 mr-1 mb-2">
-        Em análise
-      </button>
-      <button className="btn btn-elevated-warning w-24 mr-1 mb-2">
-        Pré análise
-      </button>
-      <button className="btn btn-elevated-dark w-24 mr-1 mb-2">
-        Aguardando aprovação
-      </button>
-      <button className="btn btn-elevated-dark w-24 mr-1 mb-2">
-        Aguardando atendimento
-      </button> */}
+      <span className={ClasseNameColor}>{props.situation}</span>
     </div>
   );
 }
