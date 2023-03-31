3df3d26311d76b26fba09561b2e7268c4b5879f7
omardepaiva <omar.paiva@gmail.com>
2023-03-31 16:57:29 -0300

    feat: ajuste modal

--- src/components/modalBndes/index.tsx
+++ src/components/modalBndes/index.tsx
@@ -2,14 +2,7 @@ import { Lucide, Modal, ModalBody } from "@/base-components";
 import { useState } from "react";
 export default function ModalBndes() {
   const [buttonModalPreview, setButtonModalPreview] = useState(false);
-  const [situacao, setSituacao] = useState("");
-  const [motivo, setMotivo] = useState("");
-  const [tipoApoio, setTipoApoio] = useState("");
-  const [taxaJuros, setTaxaJuros] = useState("");
-  const [taxaDesconto, setTaxaDesconto] = useState("");
-  const [valorContratado, setValorContratado] = useState("");
-  const [prazoOperacao, setPrazoOperacao] = useState("");
-  const [garantia, setGarantia] = useState("");
+
   return (
     <div>
       <div className="text-center">
@@ -41,88 +34,140 @@ export default function ModalBndes() {
             <div className="text-xl mt-5">Relatório BNDES</div>
             <div className="text-slate-500 mt-2">
               {/* inico */}
-              <div className="grid grid-cols-2 gap-1">
-                <label htmlFor="regular-form-1" className="form-label">
-                  Situação da Proposta
+              {/* coluna 1 */}
+              <div className="grid grid-cols-3 gap-4 pb-2">
+                <div>
+                  <label className="block font-medium text-gray-700 mb-2">
+                    Situação Proposta
+                  </label>
+                  <select
+                    id="select-1"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
+                    <option>Selecionar...</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                  </select>
+                </div>
+                <div>
+                  <label className="block font-medium text-gray-700 mb-2">
+                    Motivo
+                  </label>
+                  <select
+                    id="select-2"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
+                    <option>Selecionar...</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                  </select>
+                </div>
+                <div>
+                  <label className="block font-medium text-gray-700 mb-2">
+                    Tipo Apoio
+                  </label>
+                  <select
+                    id="select-2"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
+                    <option>Selecionar...</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                  </select>
+                </div>
+              </div>
+              {/* coluna 2 */}
+              <div className="grid grid-cols-3 gap-4 pb-2">
+                <div>
+                  <label className="block font-medium text-gray-700 mb-2">
+                    Taxa Juros
+                  </label>
+                  <input
+                    type="number"
+                    id="input-1"
+                    className="form-input rounded-md shadow-sm w-full"
+                  />
+                </div>
+                <div>
+                  <label className="block font-medium text-gray-700 mb-2">
+                    Taxa Desconto
+                  </label>
+                  <input
+                    type="number"
+                    id="input-2"
+                    className="form-input rounded-md shadow-sm w-full"
+                  />
+                </div>
+                <div>
+                  <label className="block font-medium text-gray-700 mb-2">
+                    Valor Contratado
+                  </label>
+                  <input
+                    type="number"
+                    id="input-3"
+                    className="form-input rounded-md shadow-sm w-full"
+                  />
+                </div>
+              </div>
+              {/* Coluna 3  */}
+              <div className="grid grid-cols-2 gap-4 pb-2">
+                <div>
+                  <label
+                    htmlFor="input-1"
+                    className="block font-medium text-gray-700 mb-2"
+                  >
+                    Prazo de operação
+                  </label>
+                  <input
+                    id="input-1"
+                    type="text"
+                    className="form-input rounded-md shadow-sm w-full"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="input-2"
+                    className="block font-medium text-gray-700 mb-2"
+                  >
+                    Prazo de Antecipação
+                  </label>
+                  <input
+                    id="input-2"
+                    type="text"
+                    className="form-input rounded-md shadow-sm w-full"
+                  />
+                </div>
+              </div>
+              {/* coluna 4 */}
+              <div className="w-full pt-2">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                  Garantias
                 </label>
                 <select
-                  className="form-control"
-                  value={situacao}
-                  onChange={(e) => setSituacao(e.target.value)}
+                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="select-multiple"
                 >
-                  <option value="">Aprovado</option>
-                  <option value="">Recusado</option>
-                  <option value="">Em-análise</option>
+                  <option>Opção 1</option>
+                  <option>Opção 2</option>
+                  <option>Opção 3</option>
+                  <option>Opção 4</option>
                 </select>
-                <label htmlFor="regular-form-1" className="form-label">
-                  Motivo
+              </div>
+              {/* área de descrição */}
+              <div className="flex flex-col pt-2">
+                <label className="mb-1 font-bold text-gray-700">
+                  Descrição:
                 </label>
-                <select
-                  className="form-control"
-                  value={motivo}
-                  onChange={(e) => setMotivo(e.target.value)}
-                >
-                  <option value="">motivo 1</option>
-                  <option value="">motivo 2</option>
-                  <option value="">motivo 3</option>
-                </select>
-                <label htmlFor="regular-form-1" className="form-label">
-                  Tipo Apoio
-                </label>
-                <select
-                  className="form-control"
-                  value={tipoApoio}
-                  onChange={(e) => setTipoApoio(e.target.value)}
-                >
-                  <option value="">opção 1</option>
-                  <option value="">opção 2</option>
-                  <option value="">opção 3</option>
-                </select>
-                <label htmlFor="regular-form-1" className="form-label">
-                  Taxa de Juros
-                </label>
-                <input
-                  className="form-control"
-                  type="number"
-                  value={taxaJuros}
-                  onChange={(e) => setTaxaJuros(e.target.value)}
-                />
-                <label htmlFor="regular-form-1" className="form-label">
-                  Taxa de Desconto
-                </label>
-                <input
-                  className="form-control"
-                  type="number"
-                  value={taxaDesconto}
-                  onChange={(e) => setTaxaDesconto(e.target.value)}
-                />
-                <label htmlFor="regular-form-1" className="form-label">
-                  Valor contratado
-                </label>
-                <input
-                  className="form-control"
-                  type="number"
-                  value={valorContratado}
-                  onChange={(e) => setValorContratado(e.target.value)}
-                />
-                <label htmlFor="regular-form-1" className="form-label">
-                  Prazo operação
-                </label>
-                <input
-                  className="form-control"
-                  type="number"
-                  value={prazoOperacao}
-                  onChange={(e) => setPrazoOperacao(e.target.value)}
-                />
-                <label htmlFor="regular-form-1" className="form-label">
-                  Garantia
-                </label>
-                <input
-                  className="form-control"
-                  type="number"
-                  value={garantia}
-                  onChange={(e) => setGarantia(e.target.value)}
-                />
+                <textarea
+                  id="description"
+                  name="description"
+                  className="p-2 border rounded-md resize-y"
+                  rows="4"
+                  placeholder="Digite aqui"
+                ></textarea>
               </div>
 
               {/* fim */}
--- src/pages/BNDESLeads/index.tsx
+++ src/pages/BNDESLeads/index.tsx
@@ -67,6 +67,7 @@ export const BndsLeads = () => {
         url="leads"
         title="Fluxo de Negociação"
       />
+
       <ModalBndes />
     </>
   );
